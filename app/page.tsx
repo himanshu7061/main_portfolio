4ceb6f3f354256f82ae20d27811307ab38dc87b8
Himanshu Kumar <himanshu.k822887@gmail.com>
2024-05-25 14:59:48 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { navItems } from "@/data";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a21013ce7ac727703dcfdee9d6582a14@o4507309756252160.ingest.us.sentry.io/4507309769228288",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
